311a90d125d488dd40df38e68d2f6f61a4849c0b
melvingonzalezjr <mgonzalez713@gmail.com>
2024-08-13 13:47:15 -0500

    Finish Part 2: Creating Menu Bar

--- src/index.js
+++ src/index.js
@@ -7,4 +7,9 @@ mainEl.classList.add("flex-ctr");
 
 //PART 2: CREATING MENU BAR
 const topMenuEl = document.getElementById("top-menu")
-topMenuEl.style = 
+topMenuEl.style.height = '100%';
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)"
+topMenuEl.classList.add("flex-around");
+
+//PART 3: ADDING MENU BUTTONS
+
