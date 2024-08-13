08894e8ac8f2a47ddd4352d19f2078e831c7b6b0
melvingonzalezjr <mgonzalez713@gmail.com>
2024-08-13 13:42:35 -0500

    Finish Part 1

---
+++ src/index.js
@@ -0,0 +1,10 @@
+
+//PART 1: GETTING STARTED
+const mainEl = document.body.querySelector("main")
+mainEl.style.backgroundColor = "var(--main-bg)"
+mainEl.innerHTML = '<h1>DOM Manipulation</h1>'
+mainEl.classList.add("flex-ctr");
+
+//PART 2: CREATING MENU BAR
+const topMenuEl = document.getElementById("top-menu")
+topMenuEl.style = 
